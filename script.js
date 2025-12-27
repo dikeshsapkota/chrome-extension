16925ab9d88a3b6777c9a6518ab4a192c8238ca7
Dikesh Sapkota <dikeshsapkota@gmail.com>
2025-12-27 16:02:29 +0545

    chrome extension updated version

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 
 let myLeads=[]
-const inputEl=document.getElementById("input-el")
 //let and const can declare variables and const cant be reassignec
+const inputEl=document.getElementById("input-el")
 const inputBtn=document.getElementById("input-btn")
+const leadsEl=document.getElementById("leads")//cache element
 inputBtn.addEventListener("click",function(){
+   
     console.log("saved")
+    myLeads.push(inputEl.value)
+    console.log(myLeads)
+   rendering();
+
 })
+function rendering(){
+    let listItems=""
+ for(let i=0;i<myLeads.length;i++){
+    
+listItems+="->"+myLeads[i]+ "\n"
+}
+leadsEl.textContent=listItems
+}
