b7001266828b18654294c3466d36c1be4301e869
Dikesh Sapkota <dikeshsapkota@gmail.com>
2025-12-27 14:14:29 +0545

    Initial commit of Chrome extension

---
+++ script.js
@@ -0,0 +1,8 @@
+
+let myLeads=[]
+const inputEl=document.getElementById("input-el")
+//let and const can declare variables and const cant be reassignec
+const inputBtn=document.getElementById("input-btn")
+inputBtn.addEventListener("click",function(){
+    console.log("saved")
+})
