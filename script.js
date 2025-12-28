a28de71d6ef08ff4e6e5274aaee4f9fddf0f9c31
Dikesh Sapkota <dikeshsapkota@gmail.com>
2025-12-28 15:51:03 +0545

    adding tags and apeending it inside li

--- script.js
+++ script.js
@@ -5,19 +5,38 @@ const inputEl=document.getElementById("input-el")
 const inputBtn=document.getElementById("input-btn")
 const leadsEl=document.getElementById("leads")//cache element
 inputBtn.addEventListener("click",function(){
-   
-    console.log("saved")
+       const lead = inputEl.value.trim() // remove extra spaces
+
+    if (lead === "") {
+    
+        return // stop execution if input is empty
+    }
+
+    console.log("saved")//to check button is working or not
     myLeads.push(inputEl.value)
-    console.log(myLeads)
+   inputEl.value=""//to clear the input field after clicking the button
 renderLeads()
 })
 
    function renderLeads(){
-    let leadlist=""
+ 
+    leadsEl.innerHTML=""
     for(let i=0;i<myLeads.length;i++){
+        //create new tag
+        const aTag=document.createElement("a")
+        aTag.href=myLeads[i]
+        aTag.target="_blank"//to open in new tab
+        leadsEl.append(aTag)
         //using template string or html in js
-        leadlist+="<li>"+myLeads[i]+"</li>"
+        //leadlist+="<li>"+myLeads[i]+"</li>" this is simple way
+        //create and append method
+        const li=document.createElement("li")
+        li.textContent=myLeads[i]
+       aTag.append(li)
+      
     }
-    leadsEl.innerHTML=leadlist
+    
+   console.log(myLeads)  //to check in the console
 }
+  
 
